8e3332c26cda0e62f89dc3258dcfb5ef58ddd40c
lucasmt <lmartinellitabajara@gmail.com>
2025-01-27 22:24:56 -0300

    Clean up ProposalOperationsTest

--- test/kontrol/ProposalOperations.t.sol
+++ test/kontrol/ProposalOperations.t.sol
@@ -31,7 +31,6 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         uint256 lastCancelledProposalId;
         Timestamp submittedAt;
         Timestamp scheduledAt;
-        //Timestamp executedAt;
         Timestamp vetoSignallingActivationTime;
     }
 
@@ -43,7 +42,6 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         pr.lastCancelledProposalId = _getLastCancelledProposalId(timelock);
         pr.submittedAt = Timestamp.wrap(_getSubmittedAt(timelock, baseSlot));
         pr.scheduledAt = Timestamp.wrap(_getScheduledAt(timelock, baseSlot));
-        //pr.executedAt = Timestamp.wrap(_getExecutedAt(timelock, baseSlot));
         pr.vetoSignallingActivationTime = Timestamp.wrap(_getVetoSignallingActivationTime(dualGovernance));
     }
 
@@ -52,7 +50,6 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         _establish(mode, pr.lastCancelledProposalId < pr.id);
         _establish(mode, pr.submittedAt != Timestamp.wrap(0));
         _establish(mode, pr.scheduledAt == Timestamp.wrap(0));
-        //_establish(mode, pr.executedAt == Timestamp.wrap(0));
     }
 
     // Validate that a scheduled proposal meets the criteria.
@@ -60,51 +57,30 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         _establish(mode, pr.lastCancelledProposalId < pr.id);
         _establish(mode, pr.submittedAt != Timestamp.wrap(0));
         _establish(mode, pr.scheduledAt != Timestamp.wrap(0));
-        //_establish(mode, pr.executedAt == Timestamp.wrap(0));
-        //_establish(mode, config.AFTER_SUBMIT_DELAY().addTo(pr.submittedAt) <= Timestamps.now());
     }
 
     function _validExecutedProposal(Mode mode, ProposalRecord memory pr) internal {
         _establish(mode, pr.lastCancelledProposalId < pr.id);
         _establish(mode, pr.submittedAt != Timestamp.wrap(0));
         _establish(mode, pr.scheduledAt != Timestamp.wrap(0));
-        //_establish(mode, pr.executedAt != Timestamp.wrap(0));
-        //_establish(mode, config.AFTER_SUBMIT_DELAY().addTo(pr.submittedAt) <= Timestamps.now());
-        //_establish(mode, config.AFTER_SCHEDULE_DELAY().addTo(pr.scheduledAt) <= Timestamps.now());
     }
 
     function _validCanceledProposal(Mode mode, ProposalRecord memory pr) internal pure {
         _establish(mode, pr.id <= pr.lastCancelledProposalId);
         _establish(mode, pr.submittedAt != Timestamp.wrap(0));
-        //_establish(mode, pr.executedAt == Timestamp.wrap(0));
     }
-
-    /*
-    function _isExecuted(ProposalRecord memory pr) internal pure returns (bool) {
-        return pr.executedAt != Timestamp.wrap(0);
-    }
-    */
 
     function _isCancelled(ProposalRecord memory pr) internal pure returns (bool) {
         return pr.lastCancelledProposalId >= pr.id;
     }
 
+    /**
+     * Test that a proposal cannot be submitted in the VetoSignallingDeactivation or VetoCooldown states.
+     */
     function testCannotProposeInInvalidState() external {
-        //_timelockStorageSetup(dualGovernance, timelock);
-        uint256 newProposalIndex = timelock.getProposalsCount();
+        uint256 proposalsCount = timelock.getProposalsCount();
 
-        address proposer = address(uint160(uint256(keccak256("proposer"))));
-        uint8 proposerIndexOneBased = uint8(kevm.freshUInt(1));
-        vm.assume(proposerIndexOneBased != 0);
-        uint160 executor = uint160(kevm.freshAddress());
-        bytes memory slotAbi = abi.encodePacked(uint88(0), uint160(executor), uint8(proposerIndexOneBased));
-        bytes32 slot;
-        assembly {
-            slot := mload(add(slotAbi, 0x20))
-        }
-        _storeBytes32(
-            address(dualGovernance), 28324487748957058971331294301258181510018269374235438230632061138814754629752, slot
-        );
+        address proposer = _getArbitraryUserAddress();
 
         State nextState = dualGovernance.getEffectiveState();
         vm.assume(nextState != State.Normal);
@@ -115,14 +91,13 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         vm.expectRevert(DualGovernance.ProposalSubmissionBlocked.selector);
         dualGovernance.submitProposal(new ExternalCall[](1), "Proposal metadata.");
 
-        assert(timelock.getProposalsCount() == newProposalIndex);
+        assert(timelock.getProposalsCount() == proposalsCount);
     }
 
     /**
      * Test that a proposal cannot be scheduled for execution if the Dual Governance state is not Normal or VetoCooldown.
      */
     function testCannotScheduleInInvalidStates(uint256 proposalId) external {
-        //_timelockStorageSetup(dualGovernance, timelock);
         _proposalIdAssumeBound(timelock, proposalId);
         _proposalStorageSetup(timelock, proposalId);
 
@@ -145,7 +120,6 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
      * Test that a proposal cannot be scheduled for execution if it was submitted after the last time the VetoSignalling state was entered.
      */
     function testCannotScheduleSubmissionAfterLastVetoSignalling(uint256 proposalId) external {
-        //_timelockStorageSetup(dualGovernance, timelock);
         _proposalIdAssumeBound(timelock, proposalId);
         _proposalStorageSetup(timelock, proposalId);
 
@@ -161,40 +135,6 @@ contract ProposalOperationsTest is DualGovernanceSetUp {
         ProposalRecord memory post = _recordProposal(proposalId);
         _validPendingProposal(Mode.Assert, post);
     }
-
-    /*
-    // Test that actions that are canceled or executed cannot be rescheduled
-    function testCanceledOrExecutedActionsCannotBeRescheduled(uint256 proposalId) external {
-        _proposalOperationsInitializeStorage(dualGovernance, timelock, proposalId);
-
-        ProposalRecord memory pre = _recordProposal(proposalId);
-        vm.assume(pre.submittedAt != Timestamp.wrap(0));
-        //vm.assume(dualGovernance.isSchedulingEnabled());
-        if (pre.state == State.VetoCooldown) {
-            vm.assume(pre.submittedAt <= pre.vetoSignallingActivationTime);
-        }
-
-        // Check if the proposal has been executed
-        if (pre.executedAt != Timestamp.wrap(0)) {
-            _validExecutedProposal(Mode.Assume, pre);
-
-            vm.expectRevert(abi.encodeWithSelector(Proposals.ProposalNotSubmitted.selector, proposalId));
-            dualGovernance.scheduleProposal(proposalId);
-
-            ProposalRecord memory post = _recordProposal(proposalId);
-            _validExecutedProposal(Mode.Assert, post);
-        } else if (pre.lastCancelledProposalId >= proposalId) {
-            // Check if the proposal has been cancelled
-            _validCanceledProposal(Mode.Assume, pre);
-
-            vm.expectRevert(abi.encodeWithSelector(Proposals.ProposalNotSubmitted.selector, proposalId));
-            dualGovernance.scheduleProposal(proposalId);
-
-            ProposalRecord memory post = _recordProposal(proposalId);
-            _validCanceledProposal(Mode.Assert, post);
-        }
-    }
-    */
 
     /**
      * Test that a proposal cannot be scheduled for execution before ProposalExecutionMinTimelock has passed since its submission.
