5b18364ff3ceda0d272cf956d772d8d476980eca
lucasmt <lmartinellitabajara@gmail.com>
2025-02-12 16:42:54 -0300

    Merge CancellingProposals tests into TimelockInvariants

--- test/kontrol/TimelockInvariants.t.sol
+++ test/kontrol/TimelockInvariants.t.sol
@@ -348,6 +348,87 @@ contract TimelockInvariantsTest is DualGovernanceSetUp {
         }
     }
 
+    /**
+     * cancelAllNonExecutedProposals cannot be called by any address other than
+     * the governance address.
+     */
+    function testOnlyGovernanceCanCancelProposals(address sender) external {
+        vm.assume(sender != timelock.getGovernance());
+
+        vm.startPrank(sender);
+
+        bytes4 errorSelector = TimelockState.CallerIsNotGovernance.selector;
+
+        vm.expectRevert(abi.encodeWithSelector(errorSelector, sender));
+        timelock.cancelAllNonExecutedProposals();
+
+        vm.stopPrank();
+    }
+
+    /**
+     * Cancelled proposals cannot be scheduled.
+     */
+    function testCancelledProposalsCannotBeScheduled(uint256 proposalId) external {
+        vm.assume(proposalId < timelock.getProposalsCount());
+
+        _proposalStorageSetup(timelock, proposalId);
+
+        Status proposalStatus = timelock.getProposalDetails(proposalId).status;
+        vm.assume(proposalStatus == Status.Cancelled);
+
+        vm.startPrank(timelock.getGovernance());
+
+        bytes4 errorSelector = ExecutableProposals.UnexpectedProposalStatus.selector;
+
+        vm.expectRevert(abi.encodeWithSelector(errorSelector, proposalId, Status.Cancelled));
+
+        timelock.schedule(proposalId);
+
+        vm.stopPrank();
+    }
+
+    /**
+     * Cancelled proposals cannot be executed.
+     */
+    function testCancelledProposalsCannotBeExecuted(uint256 proposalId) external {
+        vm.assume(proposalId < timelock.getProposalsCount());
+        vm.assume(!timelock.isEmergencyModeActive());
+
+        _proposalStorageSetup(timelock, proposalId);
+
+        Status proposalStatus = timelock.getProposalDetails(proposalId).status;
+        vm.assume(proposalStatus == Status.Cancelled);
+
+        bytes4 errorSelector = ExecutableProposals.UnexpectedProposalStatus.selector;
+
+        vm.expectRevert(abi.encodeWithSelector(errorSelector, proposalId, Status.Cancelled));
+
+        timelock.execute(proposalId);
+    }
+
+    /**
+     * Cancelled proposals cannot be emergency-executed.
+     */
+    function testCancelledProposalsCannotBeEmergencyExecuted(uint256 proposalId) external {
+        vm.assume(proposalId < timelock.getProposalsCount());
+        vm.assume(timelock.isEmergencyModeActive());
+
+        _proposalStorageSetup(timelock, proposalId);
+
+        Status proposalStatus = timelock.getProposalDetails(proposalId).status;
+        vm.assume(proposalStatus == Status.Cancelled);
+
+        vm.startPrank(timelock.getEmergencyExecutionCommittee());
+
+        bytes4 errorSelector = ExecutableProposals.UnexpectedProposalStatus.selector;
+
+        vm.expectRevert(abi.encodeWithSelector(errorSelector, proposalId, Status.Cancelled));
+
+        timelock.emergencyExecute(proposalId);
+
+        vm.stopPrank();
+    }
+
     function testSetGovernance(address newGovernance)
         external
         _checkStateRemainsUnchanged(EmergencyProtectedTimelock.setGovernance.selector)
